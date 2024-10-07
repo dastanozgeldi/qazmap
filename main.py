e598ab6357e85aedb4a2bbb9ec6a8f4b39ce1ea0
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-10-07 10:37:01 +0500

    feat: display a list of writers for different eras

--- main.py
+++ main.py
@@ -10,17 +10,29 @@ st.set_page_config(layout="wide")
 with open("data.json") as f:
     data = json.load(f)
 
-# map = folium.Map(location=data[0]['place'], zoom_start=5)
-map = folium.Map(location=[48.0196, 66.9237], zoom_start=5)
+with open("writers.json") as f:
+    writers = json.load(f)
 
-for person in data:
-    location = person['place'][0], person['place'][1]
-    folium.Marker(location, popup=generate_popup(person)).add_to(map)
+col1, col2 = st.columns([1, 3])
 
-# highlight Kazakhstan
-with open('kz.json', 'r') as f:
-    geojson_data = f.read()
+with col1:
+    with st.container(border=True):
+        for era, people in writers.items():
+            expander = st.expander(era)
+            for person in people:
+                expander.write(f'{person["name"]} ({person["lived"]})')
 
-folium.GeoJson(geojson_data).add_to(map)
+with col2:
+    map = folium.Map(location=[48.0196, 66.9237], zoom_start=5)
 
-st_folium(map, width="100%")
+    for person in data:
+        location = person['place'][0], person['place'][1]
+        folium.Marker(location, popup=generate_popup(person)).add_to(map)
+
+    # highlight Kazakhstan
+    with open('kz.json', 'r') as f:
+        geojson_data = f.read()
+
+    folium.GeoJson(geojson_data).add_to(map)
+
+    st_folium(map, width="100%")
